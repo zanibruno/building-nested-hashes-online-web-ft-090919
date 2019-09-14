3389dd431484125bca68e38847795a882e7819b0
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 13:38:12 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},{:name => "Mercution"}
+      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},{:name => "Mercution", :age => "18", :attitude => "hot-headed"}
         
       ]
    }, 
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"}
+      :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"},{:name => "Nurse", :age => "44", :attitude}
       ]
    }
 }
