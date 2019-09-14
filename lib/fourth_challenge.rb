56b127003b1ed9256dfaf49833f18557dc501ad5
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 13:44:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -2,7 +2,7 @@ def fourth_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
+      :matriarch => {name: "Lady Montague", age: 54},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},{:name => "Mercution", :age => "18", :attitude => "hot-headed"}
         
